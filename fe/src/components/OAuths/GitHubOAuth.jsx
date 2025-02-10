f4929bcc8c7d923224fbfc1a35a4de778090f363
Nguyen Phuong Tan <tannguyen.10102004@gmail.com>
2025-02-10 12:22:48 +0700

    fix(oauth): fix ui button oauth github

--- fe/src/components/OAuths/GitHubOAuth.jsx
+++ fe/src/components/OAuths/GitHubOAuth.jsx
@@ -7,9 +7,9 @@ const GithubOAuth = ({
         window.location.href = `https://github.com/login/oauth/authorize?client_id=${clientID}&redirect_uri=${redirectURI}&scope=user`;
     };
     return (
-        <button type="button" onClick={handleGithubOAuth}>
-            
-            <span>{buttonText}</span>
+        <button className="flex gap-2 bg-white items-center text-black px-3 rounded-sm" type="button" onClick={handleGithubOAuth}>
+            <span className="icon-[mdi--github] text-xl"></span>
+            <span className="text-sm text-gray-500">{buttonText}</span>
         </button>
     )
 };
